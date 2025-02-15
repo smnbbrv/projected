20cbc031e1ef98225fc50174ead4794747d26dfb
Semen Bobrov <smnbbrv@gmail.com>
2025-02-15 19:19:59 +0100

    refactor: fix file path

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@ export type * from './types/cache.js';
 
 export * from './projected-map/projected-map.js';
 export * from './projected-value/projected-value.js';
-export * from './projective-lazy-map/projected-lazy-map.js';
+export * from './projected-lazy-map/projected-lazy-map.js';
